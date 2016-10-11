5bf0d075537d2f79584258a4c67a3d73cc7f228b
Anitasol <anitaj15@ru.is>
2016-10-11 21:39:44 +0000

    Testing multiple numbers

--- src/main/java/is/ru/stringcalculator/calculator.java
+++ src/main/java/is/ru/stringcalculator/calculator.java
@@ -2,10 +2,26 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-	public static int add(String numbers){
-		if(numbers.equals(""))
+	public static int add(String text){
+		if(text.equals(""))
 			return 0;
-		else 
+
+		else if(text.contains(",")){
+			String [] numbers = text.split(",");
+			int total = 0;
+			for(String number : numbers){
+				total += toInt(number);
+			}
+			return total;
+		}
+		else	
 			return 1;
+
+
+	}
+
+	private static int toInt(String text){
+		return Integer.parseInt(text);
 	}
 }
+
